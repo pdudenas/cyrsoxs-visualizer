75f1c8c99d24c8d98b280d7cd29e4737e76721f0
pete <peter.dudenas@nist.gov>
2021-09-23 14:50:57 -0400

    fix widget test

--- cyrsoxs_visualizer/_tests/test_dock_widget.py
+++ cyrsoxs_visualizer/_tests/test_dock_widget.py
@@ -4,7 +4,7 @@ import pytest
 # this is your plugin name declared in your napari.plugins entry point
 MY_PLUGIN_NAME = "cyrsoxs-visualizer"
 # the name of your widget(s)
-MY_WIDGET_NAMES = ["LineProfiler"]
+MY_WIDGET_NAMES = ["Line Profiler"]
 
 
 @pytest.mark.parametrize("widget_name", MY_WIDGET_NAMES)
