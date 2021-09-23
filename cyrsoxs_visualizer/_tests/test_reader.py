27dbe242a7fba66251065689b4f7411150586afa
pete <peter.dudenas@nist.gov>
2021-09-23 16:21:01 -0400

    fix reader test

--- cyrsoxs_visualizer/_tests/test_reader.py
+++ cyrsoxs_visualizer/_tests/test_reader.py
@@ -28,7 +28,8 @@ def test_reader(tmp_path):
     assert isinstance(layer_data_tuple, tuple) and len(layer_data_tuple) > 0
 
     # make sure it's the same as it started
-    np.testing.assert_allclose(original_data, layer_data_tuple[0])
+    np.testing.assert_allclose(original_data_unaligned, layer_data_tuple[0])
+    np.testing.assert_allclose(original_data_aligned,layer_data_tuple[1])
 
 
 def test_get_reader_pass():
